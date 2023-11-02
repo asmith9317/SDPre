9619e162cac5081dab9109d923a7f95e84f416af
Abigail <abigail.smith27@gmail.com>
2023-11-01 22:17:18 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2{
+	public static void main(String[] args) {
+		String name = "Abigail Smith";
+		int age = 41;
+		String state = "Colorado";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(state);
+	}
+}
